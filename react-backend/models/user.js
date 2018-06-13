5d9294c6c5ca25f23d924ad1f7d9756370631801
jmelnich <julyettka@gmail.com>
2018-06-13 19:59:57 +0300

    model users

--- react-backend/models/user.js
+++ react-backend/models/user.js
@@ -1,6 +1,26 @@
 var DB = require('../DB');
 
-var db = new DB();
-var table = 'users';
 
-db.run(`CREATE TABLE ${table} (id INT, dt TEXT)`);
+
+//db.run(`CREATE TABLE ${table} (id INT, dt TEXT)`);
+
+class User {
+	constructor() {
+		this.db = new DB();
+	}
+
+	createTbl() {
+		const sql = `CREATE TABLE IF NOT EXISTS users
+		(id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email VARCHAR(255) NOT NULL,
+		username VARCHAR(30) NOT NULL,
+		firstname VARCHAR(30) NOT NULL,
+		lastname VARCHAR(30) NOT NULL,
+		password VARCHAR(255) NOT NULL,
+		activation TINYINT(1) NOT NULL DEFAULT '0',
+		sex TINYINT(1) NOT NULL,
+		avatar VARCHAR(255) DEFAULT 'public/images/avatars/default.png'
+		)`;
+	}
+
+}
