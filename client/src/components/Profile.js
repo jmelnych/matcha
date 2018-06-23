844b2e984189651a87f5f8d69365fc632397c290
jmelnich <julyettka@gmail.com>
2018-06-23 21:05:20 +0300

    style: basic styles for profile info card

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
+import Ionicon from 'react-ionicons'
 import styles from '../css/styles'
 //import avatar from '../img/avatars/default.png'
 
 export default class Profile extends Component {
   render() {
+      const ionicStyle = {
+          marginBottom: '-5px',
+          marginRight: '10px',
+          fontSize: '20px',
+      };
     return (
         <div>
 
           <div className="profile-main-header">
             <img src="" alt="profile-avatar"/>
               <figcaption>
-                <h2>Druid Wensleydale </h2>
+                <h2>Druid Wensleydale</h2>
                 <span>Accountant</span>
               </figcaption>
           </div>
@@ -20,11 +26,17 @@ export default class Profile extends Component {
             <div className="container-flex">
 
               <div className="profile-main-info">
-                <h2>Info</h2>
+                <h3>Info</h3>
+                  <p><Ionicon icon="ios-pulse-outline" color='white' style={ionicStyle}/>Rating: 10</p>
+                  <p><Ionicon icon="ios-body-outline" color='white' style={ionicStyle}/>Name: Druid Wensleydale</p>
+                  <p><Ionicon icon="ios-briefcase-outline" color='white' style={ionicStyle}/>Occupancy: Accountant</p>
+                  <p><Ionicon icon="ios-heart-outline" color='white' style={ionicStyle}/>Preferences: Men and women</p>
+                  <p><Ionicon icon="ios-time-outline" color='white' style={ionicStyle}/>Joined on October 10</p>
               </div>
 
               <div className="profile-main-feed">
-                <h2>Feed</h2>
+                <h3>Feed</h3>
+                  <p>No posts yet</p>
               </div>
 
           </div>
@@ -34,3 +46,4 @@ export default class Profile extends Component {
       )
   }
 }
+
