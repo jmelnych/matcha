f49bc5482291319216467b9075b17154d5897839
pkolomiy <pkolomiy@gmail.com>
2018-07-11 21:45:04 +0300

    Fixed activation, Added 'saveavatar' 'savephoto' and 'getphotos', Modified setup.sql

---
+++ server/controllers/ImageController/saveavatar.js
@@ -0,0 +1,17 @@
+const multer = require('multer');
+
+module.exports = (req, res) => {
+    let save     = req.app.get('save'),
+        response = multer(save.image).single('avatar'),
+        db       = req.app.get('db');
+
+    response(req, res, (err) => {
+        if (err) {
+            res.send(err);
+        } else if (req.file) {
+            let {filename} = req.file,
+                promise    = db.update('users', 'avatar', filename, 'id', req.session.id);
+            promise.then(res => res.send('Avatar updated')).catch(e => console.log(e));
+        }
+    })
+};
