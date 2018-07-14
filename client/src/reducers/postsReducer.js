134328a4e5b05195f324844b0d732607693eb7b0
Julia <julyettka@gmail.com>
2018-07-14 20:26:33 +0300

    styles: changes to post

--- client/src/reducers/postsReducer.js
+++ client/src/reducers/postsReducer.js
@@ -2,7 +2,10 @@ import {ADD_POST} from "../actions/types";
 
 const initialState = [{ id: 1,
                         text: 'Hello world',
-                        date: Date.now()}];
+                        date: Date.now()},
+    { id: 2,
+        text: 'Test',
+        date: Date.now()}];
 
 export default function (state = initialState, action){
     switch (action.type){
