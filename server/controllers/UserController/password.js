03e27e7d4b369f48340c008879f278c98a755047
pkolomiy <pkolomiy@gmail.com>
2018-07-15 15:17:54 +0300

    Added password check

--- server/controllers/UserController/password.js
+++ server/controllers/UserController/password.js
@@ -3,15 +3,19 @@ const hash = require('password-hash');
 module.exports = (req, res) => {
     let {token, password} = req.body,
         db                = req.app.get('db'),
-        promise           = db.getByUnique('users', 'token', token),
         error             = (e) => {
             console.log(e);
             res.send(e);
-        };
+        },
+        promise           = token ?
+            db.getByUnique('users', 'token', token) :
+            db.getByUnique('users', 'id', req.session.id);
 
     promise.then((response) => {
         if (!response || !response.activation || !password) {
             res.send('404');
+        } else if (!token) {
+            res.send(hash.generate(password) === response.password ? 'ok' : 'ko')
         } else {
             promise = db.update('users',
                 'password', hash.generate(password),
